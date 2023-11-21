3a1ac0f019bf5d6430fc148cd711de173a9136f5
Sughan Richardson Samson Babuji <sughanrichardson1996@gmail.com>
2023-11-21 18:32:55 -0500

    Update app.py

--- CalculatorQuantApp/app.py
+++ CalculatorQuantApp/app.py
@@ -2,14 +2,14 @@ from CalculatorQuantApp.controllers import CalculatorQuant
 
 calculator = CalculatorQuant()
 
-def lambda_handler_add(event : {} = None):
+def lambda_handler_quant_add(event : {} = None):
     resp = calculator.quant_add(**event)
     return resp
 
-def lambda_handler_subtract(event : {} = None):
+def lambda_handler_quant_subtract(event : {} = None):
     resp = calculator.quant_subtract(**event)
     return resp
 
-def lambda_handler_multiply(event : {} = None):
+def lambda_handler_quant_multiply(event : {} = None):
     resp = calculator.quant_multiply(**event)
     return resp
