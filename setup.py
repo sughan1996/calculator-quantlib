fb360ca5e3ed7699606fe5272d0f49811251d61c
Sughan Richardson Samson Babuji <sughanrichardson1996@gmail.com>
2023-11-21 21:07:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,8 @@ setup(
     author_email='sughanrichason1996@gmail.com',
     license='MIT',
     packages=find_packages(),
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
     install_requires=requirements,
     python_requires='>=3.6',
 )
-
-# setup_requires=['setuptools_scm'],
-# use_scm_version=True,
