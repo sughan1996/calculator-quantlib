a92058cf6387153591b0af15f25f52c758f1c787
Sughan Richardson Samson Babuji <sughanrichardson1996@gmail.com>
2023-11-21 20:39:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ requirements = open('requirements.txt').read().splitlines()
 
 setup(
     name='CalculatorQuantApp',
-    version='3.1.1',
     description='Your package description',
     author='Sughan R Samson',
     author_email='sughanrichason1996@gmail.com',
     license='MIT',
     packages=find_packages(),
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
     install_requires=requirements,
     python_requires='>=3.6',
 )
